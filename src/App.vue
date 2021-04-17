d94b61a795657fe2379649a7d9046119dbc856c9
arunjain9988 <arunjain9988@gmail.com>
2021-04-17 09:29:25 +0530

    Build Complete

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Url Shortening Service</h1>
+  <form >
+    <input v-model="url" type="url" name="url"  class="rounded" placeholder="Enter url to shorten">
+    <button v-on:click.prevent="onSubmit" type="submit" class="rounded">Submit</button>
+    <h3 v-if="shorted">Your Shorten Url: <a :href="`https://s-urls.herokuapp.com/${shortenurl}`" target="_blank">s-urls.herokuapp.com/{{shortenurl}}</a></h3>
+  </form>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+  },
+  data () {
+    return {
+      url:"",
+      shorted:false,
+      shortenurl:""
+    }
+  },
+  methods : {
+    onSubmit() {
+      this.axios.post('https://s-urls.herokuapp.com/?fullurl='+this.url)
+      .then((res) => {
+        console.log(res);
+        this.shorted=true;
+        this.shortenurl=res.data;
+      })
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(VueAxios,axios)
+app.mount('#app')
+
+
+
