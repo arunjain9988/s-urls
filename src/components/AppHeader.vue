4443cc594b234a204fd179d1134d464135be550e
arunjain9988 <arunjain9988@gmail.com>
2021-04-20 20:52:05 +0530

    Added Styling

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <nav class="w-full bg-gradient-to-r from-blue-700 to-blue-500 text-white py-3 px-6 text-2xl mb-4">
+    S-URLS
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
+import router from "./router/index.js";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/style.css";
 
-const app = createApp(App)
-app.use(VueAxios,axios)
-app.mount('#app')
-
-
-
+const app = createApp(App);
+app.use(VueAxios, axios);
+app.use(router);
+app.mount("#app");
